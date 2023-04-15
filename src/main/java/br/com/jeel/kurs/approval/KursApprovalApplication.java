ef44ac8c69f74d1b3116f587868ae8b88c434383
Fernando Tomasio <fernando.tomasio@sys4b.com.br>
2023-04-15 13:39:58 -0300

    first commit

---
+++ src/main/java/br/com/jeel/kurs/approval/KursApprovalApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeel.kurs.approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursApprovalApplication.class, args);
+	}
+
+}
